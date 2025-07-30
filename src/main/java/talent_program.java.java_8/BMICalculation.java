ff2e9ff5777e0cab85d2ad3672098927c10ead41
Waiyan <waiyanphyo7898@gmail.com>
2025-07-30 18:59:00 +0630

    Day 2 Assignment

---
+++ src/main/java/talent_program.java.java_8/BMICalculation.java
@@ -0,0 +1,26 @@
+package main.java.talent_program.java.java_8;
+
+import java.util.Scanner;
+
+public class BMICalculation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your weight in Kilograms");
+        double weight = sc.nextDouble();
+        System.out.println("Enter Your height in Meters");
+        double height = sc.nextDouble();
+        double result = weight/(height*height);
+        double BMI = (double) Math.round(result * 100) /100;
+        System.out.println("Your BMI is " + BMI);
+
+        if (BMI < 18.5) {
+            System.out.println("Your BMI is Underweight");
+        } else if (BMI >= 18.5 && BMI < 25) {
+            System.out.println("Your BMI is Normal weight");
+        } else if (BMI >=25 && BMI < 30) {
+            System.out.println("Your BMI is Overweight");
+        } else {
+            System.out.println("Your BMI is Obese");
+        }
+    }
+}
---
+++ src/main/java/talent_program.java.java_8/Grading.java
@@ -0,0 +1,34 @@
+package main.java.talent_program.java.java_8;
+
+import java.util.Scanner;
+
+public class Grading {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter marks for English");
+        int english = sc.nextInt();
+        System.out.println("Enter marks for Mathematics");
+        int math = sc.nextInt();
+        System.out.println("Enter marks for Chemistry");
+        int chem = sc.nextInt();
+        System.out.println("Enter marks for Physics");
+        int phys = sc.nextInt();
+        System.out.println("Enter marks for Biology");
+        int bio = sc.nextInt();
+        int total = (english + math + chem + phys + bio);
+        double average = (double) total / 5;
+        System.out.println("Your average percentage is " + average);
+
+        if(average <= 100 && average >= 90) {
+            System.out.println("You are Grade A");
+        } else if (average < 90 && average >= 80) {
+            System.out.println("You are Grade B");
+        } else if (average < 80 && average >= 70) {
+            System.out.println("You are Grade C");
+        } else if (average < 70 && average >=60) {
+            System.out.println("You are Grade D");
+        } else {
+            System.out.println("You are Grade F");
+        }
+    }
+}
