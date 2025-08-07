f050778def3bbb92c28cb3d72048a29759bc97c7
Waiyan <waiyanphyo7898@gmail.com>
2025-08-07 20:24:43 +0630

    Day 8 homework

--- src/main/java/talent_program/java/java_8/Day_8/Homework_8_2/Main.java
+++ src/main/java/talent_program/java/java_8/Day_8/Homework_8_2/Main.java
@@ -5,26 +5,62 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Printer<Integer> integerPrinter = new Printer<>();
 
-        integerPrinter.add(1);
-        integerPrinter.add(2);
-        integerPrinter.add(3);
-        integerPrinter.add(4);
-        integerPrinter.add(5);
+        IntegerPrinting();
 
-        System.out.println(" ");
-        integerPrinter.printAll();
+        doublePrinting();
+    }
+
+    private static void doublePrinting() {
 
         Printer<Double> doublePrinter = new Printer<>();
 
-        doublePrinter.add(3.00);
-        doublePrinter.add(5.39);
-        doublePrinter.add(33.20);
-        doublePrinter.add(00.43);
-        doublePrinter.add(420.29);
+        System.out.println("Enter Double values to add.");
+        System.out.println("Example : 1.11,3.33,4.33,5.02,6.40");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        String[] arrays = input.split(",");
+
+        for (String string : arrays) {
+            Double doubleValue = null;
+            try {
+                doubleValue = Double.parseDouble(string);
+            } catch (NumberFormatException e) {
+                System.out.println("Only numbers can calculated.");
+                continue;
+            }
+            doublePrinter.add(doubleValue);
+        }
 
         System.out.println(" ");
         doublePrinter.printAll();
     }
+
+    private static void IntegerPrinting() {
+
+        Printer<Integer> integerPrinter = new Printer<>();
+
+        System.out.println("Enter Integer values to add.");
+        System.out.println("Example : 1,3,4,5,6");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        String[] arrays = input.split(",");
+
+        for (String string : arrays) {
+            Integer intValue = null;
+            try {
+                intValue = Integer.parseInt(string);
+            } catch (NumberFormatException e) {
+                System.out.println("Only numbers can calculated.");
+                continue;
+            }
+            integerPrinter.add(intValue);
+        }
+
+        System.out.println(" ");
+        integerPrinter.printAll();
+    }
 }
+
--- src/main/java/talent_program/java/java_8/Day_8/Homework_8_2/Printer.java
+++ src/main/java/talent_program/java/java_8/Day_8/Homework_8_2/Printer.java
@@ -1,7 +1,6 @@
 package main.java.talent_program.java.java_8.Day_8.Homework_8_2;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Printer<T> {
 
